4748b8741f4699e951b3ff5cff001899c930f58e
SYED TAYYAB SAGHEER <syedtayyabsagheer@gmail.com>
2024-07-15 21:36:24 +0500

    resolved issues

--- app/controllers/spree/checkout_controller_decorator.rb
+++ app/controllers/spree/checkout_controller_decorator.rb
@@ -6,43 +6,36 @@ Spree::CheckoutController.class_eval do
   private
 
     def add_gift_card_payments
+      @order.add_gift_card_payments(@gift_card)
+
+      # Remove other payment method parameters.
       params[:order].delete(:payments_attributes)
       params.delete(:payment_source)
-      adjustment_amount = [@order.total, @gift_card.amount_remaining].min
-      if @gift_card.amount_remaining.to_f > 0.0
-        if @order.total > @gift_card.amount_remaining
-          @order.adjustments.create!(label: 'GIFT CARD', amount: -adjustment_amount, order: @order, source: @gift_card)
-          @order.update_with_updater!
-          @gift_card.debit(adjustment_amount)
-          redirect_to checkout_state_path(@order.state) and return
-        else
-          @order.next
-          @order.adjustments.create!(label: 'GIFT CARD', amount: -adjustment_amount,order: @order)
-          @order.update_with_updater!
-          @order.update(state: 'complete', completed_at: Time.current)
-          handle_zero_amount_payment
-          @order.finalize!
-          redirect_to completion_route
-        end
-        @gift_card.debit(adjustment_amount)
-      else
+
+      # Return to the Payments page if additional payment is needed.
+      if @order.payments.valid.sum(:amount) < @order.total
         redirect_to checkout_state_path(@order.state) and return
       end
     end
 
     def remove_gift_card_payments
-      if params.key?(:remove_gift_card) && @order.using_gift_card?
-        @order.remove_gift_card_payments
-        redirect_to checkout_state_path(@order.state) and return
+      unless params[:state] == "confirm"
+        if params.key?(:remove_gift_card) && @order.using_gift_card?
+          @order.remove_gift_card_payments
+          redirect_to checkout_state_path(@order.state) and return
+        end
       end
     end
 
     def payment_via_gift_card?
-      params[:state] == "payment" && params[:order].fetch(:payments_attributes, {}).present? && params[:order][:payments_attributes].select { |payments_attribute| gift_card_payment_method.try(:id).to_s == payments_attribute[:payment_method_id] }.present?
+      unless params[:state] == "confirm"
+        params[:state] == "payment" && params[:order].fetch(:payments_attributes, {}).present? && params[:order][:payments_attributes].select { |payments_attribute| gift_card_payment_method.try(:id).to_s == payments_attribute[:payment_method_id] }.present?
+      end
     end
 
     def load_gift_card
       @gift_card = Spree::GiftCard.find_by(code: params[:payment_source][gift_card_payment_method.try(:id).to_s][:code])
+
       unless @gift_card
         redirect_to checkout_state_path(@order.state), flash: { error: Spree.t(:gift_code_not_found) } and return
       end
@@ -51,25 +44,4 @@ Spree::CheckoutController.class_eval do
     def gift_card_payment_method
       @gift_card_payment_method ||= Spree::PaymentMethod.gift_card.available.first
     end
-
-    def handle_zero_amount_payment
-    if @order.total == 0
-      payment_method = Spree::PaymentMethod.find_by(type: 'Spree::PaymentMethod::GiftCard') # or your specific payment method
-      create_zero_amount_payment(@order, payment_method)
-    end
-  end
-
-  def create_zero_amount_payment(order, payment_method)
-    payment = order.payments.create!(
-      amount: 0.0,
-      payment_method: payment_method,
-      state: 'completed',
-      source: @gift_card
-    )
-
-    unless payment.persisted?
-      flash[:error] = "Payment creation failed"
-      redirect_to checkout_state_path(order.state) and return
-    end
-  end
 end
--- app/models/spree/order_contents_decorator.rb
+++ app/models/spree/order_contents_decorator.rb
@@ -1,22 +1,8 @@
 Spree::OrderUpdater.class_eval do
-  def grab_line_item_by_variant(variant, raise_error = false, options = {})
-    line_item = order.find_line_item_by_variant(variant)
-    line_item ||= order.line_items.detect { |li| li.variant_id == variant.id && options_match?(li.options, options) }
-    line_item
-  end
-
   def grab_line_item_by_variant_with_gift_card(variant, raise_error = false, options = {})
     return if variant.product.is_gift_card?
 
     grab_line_item_by_variant_without_gift_card(variant, raise_error, options)
   end
-
-  alias_method :grab_line_item_by_variant_without_gift_card, :grab_line_item_by_variant
-  alias_method :grab_line_item_by_variant, :grab_line_item_by_variant_with_gift_card
-
-  private
-
-  def options_match?(existing_options, new_options)
-    existing_options.symbolize_keys == new_options.symbolize_keys
-  end
+  alias_method :orig_grab_line_item_by_variant, :grab_line_item_by_variant_with_gift_card
 end
