3d7b3fe7bd36853e38d3b203f514b84fc3388b3e
Omair Azam <omairrr.azam@gmail.com>
2024-08-21 00:23:19 +0500

    Short the Length of gift card

--- app/models/spree/gift_card.rb
+++ app/models/spree/gift_card.rb
@@ -219,7 +219,7 @@ module Spree
 
     def generate_code
       until self.code.present? && self.class.where(code: self.code).count == 0
-        self.code = Digest::SHA1.hexdigest([Time.now, rand].join)
+        self.code  = Digest::SHA1.hexdigest([Time.now, rand].join)[0..7]
       end
     end
 
