46d42ee3a03459ad2651099ad7c91595e9082df0
Omair Azam <omairrr.azam@gmail.com>
2024-08-21 15:16:46 +0500

    Chore: Move Email Transaction of Gift Card

--- app/models/spree/payment_decorator.rb
+++ app/models/spree/payment_decorator.rb
@@ -6,16 +6,16 @@ module Spree
 
         delegate :gift_card?, to: :payment_method, allow_nil: true
 
-        state_machine.after_transition to: :completed, do: :process_gift_card
+        #state_machine.after_transition to: :completed, do: :process_gift_card
 
-        def process_gift_card
-          order.line_items.each do |li|
-            if li.gift_card
-              li.gift_card.update_column(:enabled, true)
-              Spree::OrderMailer.gift_card_email(li.gift_card.id, order).deliver_later
-            end
-          end
-        end
+        # def process_gift_card
+        #   order.line_items.each do |li|
+        #     if li.gift_card
+        #       li.gift_card.update_column(:enabled, true)
+        #       Spree::OrderMailer.gift_card_email(li.gift_card.id, order).deliver_later
+        #     end
+        #   end
+        # end
 
         def store_credit_or_gift_card?
           store_credit? || gift_card?
