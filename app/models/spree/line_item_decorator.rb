f1d4019310f6a976b0acad74b1ce223388467d8a
Omair Azam <omairrr.azam@gmail.com>
2025-05-08 18:08:16 +0500

    Fix: remove payment of gift card

--- app/models/spree/line_item_decorator.rb
+++ app/models/spree/line_item_decorator.rb
@@ -1,19 +1,25 @@
 module Spree
 	module LineItemDecorator
 	  MAXIMUM_GIFT_CARD_LIMIT = 1
-  
-	  def self.prepended(base)
-	   base.has_one :gift_card, dependent: :destroy
-  
-	   # base.with_options if: :is_gift_card? do
-		#  base.validates :gift_card, presence: true
-		#  base.validates :quantity, numericality: { less_than_or_equal_to: Spree::LineItemDecorator::MAXIMUM_GIFT_CARD_LIMIT }, allow_nil: true
-	   # end
-  
-	   # base.delegate :is_gift_card?, to: :product
+
+		def self.prepended(base)
+	   	base.has_one :gift_card, dependent: :destroy
+		 	base.after_destroy :remove_gift_card_payment
 	  end
+
+		private
+
+		def remove_gift_card_payment
+      return unless gift_card?
+			order.payments.gift_cards.checkout.map(&:invalidate!)
+    end
+
+    def gift_card?
+			product = variant.product
+			product.is_gift_card? || product.variants.any? { |item| item.is_specific_gift_card? }
+		end
+
 	end
-  end
-  
-  Spree::LineItem.prepend(Spree::LineItemDecorator)
-  
+end
+
+Spree::LineItem.prepend(Spree::LineItemDecorator)
